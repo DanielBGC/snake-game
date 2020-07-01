2ddc26cc9d32387a312ed102e5d472dfa31bc4ef
Daniel Borba Guimarães da Costa <daniel.borba-@hotmail.com>
2020-07-01 20:42:55 -0300

    Adicionando a tela de gameover para dispositivos móveis

--- scripts/gameover.js
+++ scripts/gameover.js
@@ -13,14 +13,13 @@ function GameOver() {
     this.draw = function(winner) {
         ctx.fillStyle = "#5d8357";
         ctx.font = tileSize * 2 + "px Arial";
-        
-        if(player == 1) {
-            ctx.fillText(this.messages["gameover"], WIDTH / 2 - ctx.measureText(this.messages["gameover"]).width / 2, HEIGHT / 2)   
-            ctx.fillText(this.messages["click"], WIDTH / 2 - ctx.measureText(this.messages["click"]).width / 2, HEIGHT / 5 * 3)   
-        }
-
+    
         if(player == 2) {
             ctx.fillText(this.messages[winner], WIDTH / 2 - ctx.measureText(this.messages[winner]).width / 2, HEIGHT / 2)   
+            ctx.fillText(this.messages["click"], WIDTH / 2 - ctx.measureText(this.messages["click"]).width / 2, HEIGHT / 5 * 3)   
+        }
+        else {
+            ctx.fillText(this.messages["gameover"], WIDTH / 2 - ctx.measureText(this.messages["gameover"]).width / 2, HEIGHT / 2)   
             ctx.fillText(this.messages["click"], WIDTH / 2 - ctx.measureText(this.messages["click"]).width / 2, HEIGHT / 5 * 3)   
         }
 
